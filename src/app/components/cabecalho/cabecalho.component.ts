68a7eb79a88f5b8d2276b1519d3a9062a503357c
jooemanoel <jooemanoel@gmail.com>
2024-10-05 00:00:48 -0300

    Botão de voltar e mais

--- src/app/components/cabecalho/cabecalho.component.ts
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
   styleUrls: ['./cabecalho.component.css']
 })
-export class CabecalhoComponent {
+export class CabecalhoComponent implements OnInit {
+  tabela = true;
   menuAberto = false;
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log(event.url);
+        if (event.url === '/tabela') this.tabela = true;
+        if (event.url === '/formulario') this.tabela = false;
+      }
+    });
+  }
   abrir() {
     this.menuAberto = true;
   }
